dcbbdbe773d96eb15a071348b3fb635ffd460aa8
ss.kim <tjdtn94k@naver.com>
2023-12-08 23:41:06 +0900

    project board complete

---
+++ project-board/src/main/java/com/projectboard/config/DataRestConfig.java
@@ -0,0 +1,25 @@
+package com.projectboard.config;
+
+import com.projectboard.domain.Article;
+import com.projectboard.domain.ArticleComment;
+import com.projectboard.domain.Hashtag;
+import com.projectboard.domain.UserAccount;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class DataRestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return RepositoryRestConfigurer.withConfig((config, cors) ->
+                config
+                        .exposeIdsFor(UserAccount.class)
+                        .exposeIdsFor(Article.class)
+                        .exposeIdsFor(ArticleComment.class)
+                        .exposeIdsFor(Hashtag.class)
+        );
+    }
+
+}
---
+++ project-board/src/main/java/com/projectboard/dto/request/ArticleRequest.java
@@ -0,0 +1,31 @@
+package com.projectboard.dto.request;
+
+import com.projectboard.dto.ArticleDto;
+import com.projectboard.dto.HashtagDto;
+import com.projectboard.dto.UserAccountDto;
+
+import java.util.Set;
+
+public record ArticleRequest(
+        String title,
+        String content
+) {
+
+    public static ArticleRequest of(String title, String content) {
+        return new ArticleRequest(title, content);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return toDto(userAccountDto, null);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto, Set<HashtagDto> hashtagDtos) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtagDtos
+        );
+    }
+
+}
